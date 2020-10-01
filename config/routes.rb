e86a2f21e4b98212e73b00b7c6d0278264f0e314
Hugo Santos <hugoee@gmail.com>
2020-10-01 17:15:33 -0300

    Add a improve in the view index of the personal data

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   #, :path => 'accounts', :controllers => { registrations: 'registrations'}
 
-  resources :personal_data, only: [:index]
+  resources :personal_data, only: [:index, :show]
 
   get '/user' => "users#home", :as => :user_root
 
