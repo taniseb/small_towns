23a045d78ff1723e08db4c23bf5d8df2892f3a12
tanise bussmann <tanisebrandao@gmail.com>
2020-10-02 16:50:05 -0300

    parte inicial controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  
+
     def show
     end
-  
+    def home
+      @requisitions_pendente = Requisition.where(status:"pendente")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show] do
-    resources :requisitions, only: [:index,:new,:create]
+    resources :requisitions, only: [:index,:new,:create] do
+      patch '/change_status', to: "requisitions#change_status", as: :change_status
+    end
     resources :personal_data, only: [:index, :show]
   end
+
 
 
   get '/user' => "users#home", :as => :user_root
