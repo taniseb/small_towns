01f0508c00b6752a2d487d8750385222508da694
tanise bussmann <tanisebrandao@gmail.com>
2020-10-07 16:03:38 -0300

    clounary + retirar o texto do sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_one_attached :photo
   has_many :requisitions, dependent: :destroy
   has_many :personal_data
   belongs_to :city_hall, optional:true
