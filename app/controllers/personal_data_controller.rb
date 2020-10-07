c92e7ed466aa46224556b4fad69943949bc6187f
tanise bussmann <tanisebrandao@gmail.com>
2020-10-07 17:53:03 -0300

    edit funcionando

--- app/controllers/personal_data_controller.rb
+++ app/controllers/personal_data_controller.rb
@@ -7,30 +7,7 @@ class PersonalDataController < ApplicationController
 
   def show; end
 
-  def destroy
-    @user = User.find(params[:user_id])
-    @requisition = Requisition.where(user_id: @user.id, status: "aprovada", excluded: true)
-    @personal_datum = []
-    @requisition.each do |r|
-      @personal_datum << PersonalDatum.find_by(datum_font: r.field_name)
-    end
-    @personal_datum.each {|p| p.destroy}
-    redirect_to user_root, notice: 'Dados foram deletados.'
-  end
 
-  def update
-    @user = User.find(params[:user_id])
-    @requisition = Requisition.where(user_id: @user.id, status: "aprovada").where.not(new_value: nil)
-    @requisition.each do |r|
-      @personal_datum = PersonalDatum.find_by(datum_font: r.field_name)
-    end
-    @personal_datum.datum_information = @requisition.new_value
-    if personal_datum.update(personal_datum_params)
-      redirect to user_root, notice: 'Dados atualizados'
-    else
-      redirect to user_root, notice: 'Não foi possível atualizar'
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
