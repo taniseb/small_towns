77c46a5f645445967187431543ab78fe36390dbd
tanise bussmann <tanisebrandao@gmail.com>
2020-10-08 12:34:10 -0300

    mudanças da manhã

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :requisitions, dependent: :destroy
   has_many :personal_data
   belongs_to :city_hall, optional:true
+  validates :first_name, format: { with: /\A[a-zA-Zá-úÁ-ÚüÜ]+\z/ }
+  validates :last_name, format: { with: /\A[a-zA-Zá-úÁ-ÚüÜ]+\z/  }
+  validates :cpf, length: { is: 11 }, format: { with: /[0-9]/}
+
   def avaliable_data
     PersonalDatum.joins(:requisitions)
         .where(requisitions:{status: "aprovada"}, user_id: self.id)
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,24 @@ Requisition.delete_all
 
 puts "---------------------------"
 
-c = CityHall.new(name: "Goiania", ibge_code: "159753")
+c = CityHall.new(name: "Santo Rails", ibge_code: "159753")
 
 c.save!
+s = User.new(email: admin@admin.com , first_name: "Admin" last_name:"Admin",
+    cpf: Faker::Number.leading_zero_number(digits: 11), password: "123456", password_confirmation: "123456",
+    city_hall: c, datum_admin:true)
+s.save!
 
 10.times do
   u = User.new(email: Faker::Internet.email , first_name: Faker::Name.first_name, last_name:Faker::Name.last_name,
     cpf: Faker::Number.leading_zero_number(digits: 11), password: "123456", password_confirmation: "123456",
     city_hall: c)
-
   u.save!
-
 
   5.times do
     p = PersonalDatum.new(datum_font: ["IPTU", "ITBI", "ISS", "Dados Escolares", "Dados de Saúde"].sample, datum_information: Faker::Lorem.sentence(word_count: 5), datum_access: ["Gestor", "Servidor", "Fornecedor"].sample, city_hall: c, user: u)
     p.save!
-    r = Requisition.new(status: ["pendente", "autorizada", "negado"].sample, field_name: ["IPTU", "ITBI", "ISS", "Dados Escolares", "Dados de Saúde"].sample, new_value: Faker::Lorem.sentence(word_count: 5), justification: Faker::Lorem.sentence(word_count: 5), excluded: [false, true].sample, user: u, personal_datum: p)
+    r = Requisition.new(status: ["pendente", "aprovada", "reprovada"].sample, field_name: ["IPTU", "ITBI", "ISS", "Dados Escolares", "Dados de Saúde"].sample, new_value: Faker::Lorem.sentence(word_count: 5), justification: Faker::Lorem.sentence(word_count: 5), excluded: [false, true].sample, user: u, personal_datum: p)
     r.save!
   end
 
